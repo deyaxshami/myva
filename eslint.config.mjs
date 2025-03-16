649310f1da94cdd041c3a18d547777b9f9153f04
deyaxshami <deya.shami@icloud.com>
2025-03-16 16:05:12 +0100

    eslint updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,8 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unused-vars": "off", // Disable TypeScript unused vars error
       "unused-imports/no-unused-imports": "off", // Disable unused imports error
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      "prefer-const": "off"
     },
   },
 ];
